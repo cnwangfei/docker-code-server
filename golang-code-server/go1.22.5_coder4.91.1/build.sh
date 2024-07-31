4a0ebaa3a315ae4db8fc1ba178a3f6a53ded2753
cnWangFei <cnwangfei@github.com>
2024-07-31 17:49:17 +0800

    create code-server:go1.22.5_coder4.91.1

---
+++ golang-code-server/go1.22.5_coder4.91.1/build.sh
@@ -0,0 +1,8 @@
+# 停止所有的container 
+docker stop $(docker ps -a -q)
+# 删除所有container
+docker rm $(docker ps -a -q)
+# 删除none镜像
+docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
+# 创建镜像
+docker build --force-rm --no-cache -t cnwangfei/code-server:go1.22.5_coder4.91.1 . --build-arg GOVERSION=1.22.5
