a4d48627c86738c2356dd0b33cc6fc6319da4a48
cnWangFei <cnwangfei@github.com>
2024-08-02 16:36:28 +0800

    update code-server:go1.22.5_coder4.91.1_20240801

--- golang-code-server/go1.22.5_coder4.91.1/build.sh
+++ golang-code-server/go1.22.5_coder4.91.1/build.sh
@@ -5,4 +5,5 @@ docker rm $(docker ps -a -q)
 # 删除none镜像
 docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
 # 创建镜像
-docker build --force-rm --no-cache -t cnwangfei/code-server:go1.22.5_coder4.91.1 . --build-arg GOVERSION=1.22.5
+current_date=$(date +'%Y%m%d')
+docker build --force-rm --no-cache -t cnwangfei/code-server:go1.22.5_coder4.91.1_${current_date} . --build-arg GOVERSION=1.22.5
